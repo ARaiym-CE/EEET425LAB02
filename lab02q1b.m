b6217ae4bd6bc0cc8dbfbe4ccf415211af20f838
Ali Raiymkulov <ar5310@rit.edu>
2026-09-16 19:55:15 -0400

    kuliza

--- lab02q1b.m
+++ lab02q1b.m
@@ -3,24 +3,25 @@
 n = 0:99; %time
 
 h1 = [1, 1/2, 1/4, 1/8, 1/16, 1/32];
-
 h2 = [1, 1, 1, 1, 1];
 
 h1h2 = conv(h1, h2);
 
 h3 = [1/4, 1/2, 1/4];
 
-h1h2h3 = conv(h1h2, h3);
+hlength = length(h1h2);
 
-x = [0.1 * cos(0.2 * pi * n) + (0.95 .^ n) .* cos(1/3 * pi * n)]; %T4 impulse response
+h1h2h3 = h1h2 + [h3, zeros(1, hlength - length(h3))]; %parallel so adding up and down also filling in zeros to make the same size
 
 a4 = [1 -0.9 0.81];
 
 b4 = [1 2];
 
-x123 = [h1h2h3 zeros(1, 100-length(h1h2h3))];
+s_in = 0.1 * cos(0.2 * pi * n) + (0.95 .^ n) .* cos((1/3) * pi * n); %input sequence
 
-h = filter(b4, a4, x123);
+x123 = conv(s_in, h1h2h3); 
+
+h = filter(b4, a4, x123(1:100));
 
 stem(n, h, 'filled')
 
